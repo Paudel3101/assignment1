6fc6be1198f3aa0e0e02816f50c8064e24aeb7d5
Paudel3101 <bishalsharma24112002@gmail.com>
2025-09-24 17:21:37 -0400

    Initial Commit

---
+++ assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by Bishal Paudel on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
